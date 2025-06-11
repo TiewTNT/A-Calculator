e6ad3a828ed863fed69c86d8d2a1e646417d1a47
Tiew <tnttiew@gmail.com>
2025-06-11 19:46:18 +1000

    no more debug runs!

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from sympy import pi, E, oo, Symbol
 
 import sympy
 import random
+import os
 
 app = Flask(__name__)
 
@@ -78,4 +79,5 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
